bc68abe6c7797d901bc834e48adb8c3baffcbf27
SinthuPriya <sinthuja.engg@gmail.com>
2023-08-12 15:25:27 +0530

    Hello

---
+++ src/org/hello/Welecom.java
@@ -0,0 +1,13 @@
+package org.hello;
+
+public class Welecom {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Welcome to a new Superstar");
+		System.out.println("Welcome to a Gangstar");
+		
+	}
+
+}
